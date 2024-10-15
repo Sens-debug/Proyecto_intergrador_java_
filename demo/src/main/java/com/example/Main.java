04cf89fa3ae4c6bff8b5c0c74bb9e3a2c845e9b5
Sense <jsant@cesde>
2024-10-15 14:46:48 -0500

    agrego funcionalidad (formularios ingreso-salida(usuarios previamente registrados), agrego funcionalidad (visualizacion horarios almacenados(desde usuario administrador)

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -9,6 +11,10 @@ public class Main {
         Scanner teclado = new Scanner(System.in);
         ArrayList<String> users = new ArrayList<>(); // contenedor iterable de la credencial "usuario"
         ArrayList<String> passwords = new ArrayList<>(); // contenedor iterable de la credencial "contraseña"
+
+        HashMap<String, ArrayList<Integer>> map = new HashMap<>(); // Diccionario de datos (almacenaje horas, nombres)
+        users.add("ADMINISTRAD0R");
+        passwords.add("ADMINISTRAD0R");
         boolean centinelaW = true;
         while (centinelaW) {
             System.out.println("Ingrese una opcion valida dentro del formulario \n");
@@ -26,12 +32,12 @@ public class Main {
                         user = input.nextLine();
                         users.add(user);
                         System.out.println("Usuario valido");
-                   
+
                         System.out.println("Ingrese la contraseña a registrar");
                         password = input.nextLine();
                         passwords.add(password);
 
-                    }catch (Exception e) {
+                    } catch (Exception e) {
                         System.out.println("ERROR EN EL REGISTRO; CRENDECIALES INVALIDAS.\n vuelva a intentar");
                         break;
                     }
@@ -46,13 +52,89 @@ public class Main {
                     tryUser = input2.nextLine();
                     System.out.println("INGRESE CONTRASEÑA");
                     tryPassword = input2.nextLine();
-                    for (int i = 0; i <= passwords.size(); i++) {
+                    boolean centinelaWhileUsers = true;
+                    for (int i = 1; i <= passwords.size(); i++) {
 
                         try {
                             if (users.get(i).equals(tryUser) && passwords.get(i).equals(tryPassword)) {
                                 System.out.println("INICIO DE SESION EXITOSO\n");
                                 System.out.println("Bienvenido " + users.get(i) + "\n");
+                                while (centinelaWhileUsers) {
+                                    ArrayList<Integer> temporal = new ArrayList<>();
+                                    System.out.println(
+                                            "El sistema desplegará una interfaz que le solicitará sus horarios");
+                                    System.out.println("1.REGISTRO DE HORAS");
+                                    System.out.println("2.SALIR DE INTERFAZ");
+                                    Scanner teclado2 = new Scanner(System.in);
+                                    int centinelaUsers = teclado2.nextInt();
+                                    switch (centinelaUsers) {
+                                        case 1:
+                                            System.out.println("1 ---> INGRESAR HORA INGRESO");
+                                            System.out.println(
+                                                    "ESCRIBA LA HORA DE INGRESO (FORMATO 24HR's) (horas enteras)");
+                                            Integer horaIn = teclado2.nextInt();
+                                            if (horaIn > 24) {
+                                                System.out.println("Rango horario invalido");
+                                                break;
+
+                                            } else {
+                                                temporal.add(horaIn);
+                                            }
+
+                                            System.out.println("2 ---> INGRESAR HORA SALIDA");
+                                            System.out.println("ESCRIBA LA HORA DE SALIDA (FORMATO 24HR's)");
+                                            Integer horaOut = teclado2.nextInt();
+                                            if (horaOut > 24) {
+                                                System.out.println("Rango horario invalido");
+                                                break;
+
+                                            } else {
+                                                temporal.add(horaOut);
+                                            }
+                                            map.put(users.get(i), temporal);
+                                            break;
+
+                                        case 2:
+                                            System.out.println("GRACIAS\nSALIENDO");
+                                            centinelaWhileUsers = false;
+                                            break;
+
+                                        default:
+                                            System.out.println("OPCION INVALIDA");
+                                            break;
+                                    }
+
+                                }
                                 break;
+                            } else if (users.get(0).equals(tryUser) && passwords.get(0).equals(tryPassword)) {
+                                System.out.println("SESION DE ADMINISTRADOR");
+                                boolean centinelaWhileAdmin = true;
+                                while (centinelaWhileAdmin) {
+                                    Scanner teclado2 = new Scanner(System.in);
+                                    System.out.println("QUE OPCION QUIERE REALIZAR?");
+                                    System.out.println("1.MOSTRAR REGISTROS DE EMPLEADOS");
+                                    System.out.println("2.Salir de la interfaz");
+                                    int centinelaAdmin = teclado2.nextInt();
+                                    switch (centinelaAdmin) {
+                                        case 1:
+                                            for (Map.Entry<String, ArrayList<Integer>> entry : map.entrySet()) {
+                                                String clave = entry.getKey();
+                                                ArrayList<Integer> listaHoras = entry.getValue();
+                                                System.out.println("USUARIO " + clave + ", HORAS (INGRESO, SALIDA) " + listaHoras);
+                                            }
+                                            break;
+                                        case 2:
+                                            System.out.println("Gracias por unas la interfaz\nSALIENDO");
+                                            centinelaWhileAdmin=false;
+                                            break;
+
+                                        default:
+                                        System.out.println("OPCION INVALIDA");
+                                            break;
+                                    }
+
+                                }
+
                             } else {
                                 continue;
                             }
@@ -70,8 +152,8 @@ public class Main {
                     centinelaW = false;
                     break; // break "case 3"
 
-                default : 
-                System.out.println("ingrese una opcion valida dentro del menu\n");
+                default:
+                    System.out.println("ingrese una opcion valida dentro del menu\n");
 
             }
 
